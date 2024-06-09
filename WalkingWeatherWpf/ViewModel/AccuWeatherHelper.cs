1472ec24f8c7cdeea33152c8373b0882b5e72dcd
Meng-Hsin Chen <walkproudly@gmail.com>
2024-06-09 17:57:37 +0800

    Create ViewModels

---
+++ WalkingWeatherWpf/ViewModel/AccuWeatherHelper.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WalkingWeatherWpf.Model;
+
+namespace WalkingWeatherWpf.ViewModel
+{
+    public static class AccuWeatherHelper
+    {
+        private const string BASE_URL = "http://dataservice.accuweather.com/";
+
+        private static readonly string API_KEY = SecretHelper.ReadIniValue("AccuWeather", "ApiKey");
+
+        public static List<City> GetCities(string query)
+        {
+            List<City> cities = new List<City>();
+            string url = $"{BASE_URL}locations/v1/cities/autocomplete?apikey={API_KEY}&q={query}";
+            using(HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                string json = response.Content.ReadAsStringAsync().Result;
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+            return cities;
+        }
+
+        public static CurrentConditions GetCurrentConditions(string locationKey)
+        {
+            CurrentConditions currentConditions = new CurrentConditions();
+            string url = $"{BASE_URL}currentconditions/v1/{locationKey}?apikey={API_KEY}";
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                string json = response.Content.ReadAsStringAsync().Result;
+                currentConditions = JsonConvert.DeserializeObject<List<CurrentConditions>>(json).FirstOrDefault();
+            }
+            return currentConditions;
+        }
+    }
+}
---
+++ WalkingWeatherWpf/ViewModel/SecretHelper.cs
@@ -0,0 +1,24 @@
+﻿using IniParser.Model;
+using IniParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkingWeatherWpf.ViewModel
+{
+    public static class SecretHelper
+    {
+        private static string iniPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Secrets.ini");
+
+        private static FileIniDataParser iniParser = new FileIniDataParser();
+
+        public static string ReadIniValue(string section, string key)
+        {
+            IniData data = iniParser.ReadFile(iniPath);
+            return data[section][key];
+        }
+    }
+}
