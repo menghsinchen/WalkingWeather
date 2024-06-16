a71bb6c4ff26e1c6fec35f1654c5d1d8379ca314
Meng-Hsin Chen <walkproudly@gmail.com>
2024-06-16 10:57:16 +0800

    Create WeatherViewModel and binding the View

---
+++ WalkingWeatherWpf/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalkingWeatherWpf.Model;
+
+namespace WalkingWeatherWpf.ViewModel
+{
+    public class WeatherViewModel : INotifyPropertyChanged
+    {
+        private string query;
+
+        private CurrentConditions currentConditions;
+
+        private City selectedCity;
+
+        public WeatherViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City()
+                {
+                    LocalizedName = "Taipei"
+                };
+                CurrentConditions = new CurrentConditions()
+                {
+                    WeatherText = "Partly cloudy",
+                    Temperature = new Temperature()
+                    {
+                        Metric = new Units()
+                        {
+                            Value = 25
+                        }
+                    }
+                };
+            }
+        }
+
+        public string Query
+        {
+            get { return query; }
+            set
+            {
+                query = value;
+                OnPropertyChanged(nameof(Query));
+            }
+        }
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set
+            {
+                currentConditions = value;
+                OnPropertyChanged(nameof(CurrentConditions));
+            }
+        }
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged(nameof(SelectedCity));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
