2c499809679c33ff05d930bb578f172b797e75ee
Meng-Hsin Chen <walkproudly@gmail.com>
2024-06-30 17:12:25 +0800

    Implement IValueConverter

---
+++ WalkingWeatherWpf/ViewModel/BoolToRainConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WalkingWeatherWpf.ViewModel
+{
+    public class BoolToRainConverter : IValueConverter
+    {
+        private const string CURRENTLY_RAINING = "Currently raining";
+        private const string CURRENTLY_NOT_RAINING = "Currently not raining";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isRaining = (bool)value;
+            return isRaining ? CURRENTLY_RAINING : CURRENTLY_NOT_RAINING;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string isRaining = value.ToString();
+            return isRaining == CURRENTLY_RAINING ? true : false;
+        }
+    }
+}
